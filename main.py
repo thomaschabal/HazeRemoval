67765b7d8a145d38ab494d4ae766645728e49be0
Martin Graive <martin.graive@eleves.enpc.fr>
2021-02-01 21:17:21 +0100

    Add argument for matting

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ from haze_removal import HazeRemover, load_image, show_imgs, PATCH_SIZE
 parser = argparse.ArgumentParser(description="Haze removal function")
 parser.add_argument("--path", "-p", type=str, help="Image path", default="./images/20210127_142511.jpg")
 parser.add_argument("--patch_size", type=int, help="Patch size for dark channel extraction", default=PATCH_SIZE)
-parser.add_argument("--height", type=int, help="Image height", default=500)
+parser.add_argument("--resize", type=int, help="Size of the largest side", default=1000)
 parser.add_argument("--save_folder", "-s", type=str, help="Folder to save haze-free image in", default="./results/")
+parser.add_argument("--matting", action="store_true", help="Whether to use soft matting")
 args = parser.parse_args()
 
 if not os.path.exists(args.save_folder):
@@ -22,7 +23,7 @@ name, extension = file_name.split('.')
 print(f"Loading image from {args.path}")
 image = load_image(args.path, args.height, show_image=False)
 
-haze_remover = HazeRemover(image, patch_size=args.patch_size, matting=False)
+haze_remover = HazeRemover(image, patch_size=args.patch_size, matting=args.matting)
 radiance, transmission, _ = haze_remover.remove_haze(.6)
 
 plt.imsave(args.save_folder + f"{name}_radiance.{extension}", radiance)
