ee956bd167ae9872d1a789d855cd46adaba0b95f
Martin Graive <martin.graive@eleves.enpc.fr>
2021-02-03 23:18:41 +0100

    Add parser args

--- main.py
+++ main.py
@@ -2,12 +2,16 @@
 
 import argparse
 import matplotlib.pyplot as plt
-from haze_removal import HazeRemover, load_image, show_imgs, PATCH_SIZE, create_save_folder_and_get_file_info, str2bool, get_save_extension, LAMBDA
+from haze_removal import HazeRemover, load_image, show_imgs, PATCH_SIZE, create_save_folder_and_get_file_info, str2bool, get_save_extension, LAMBDA, T0, OMEGA, OPAQUE
 
 parser = argparse.ArgumentParser(description="Haze removal function")
 parser.add_argument("--path", "-p", type=str, help="Image path", default="./images/20210127_142511.jpg")
 parser.add_argument("--patch_size", type=int, help="Patch size for dark channel extraction", default=PATCH_SIZE)
 parser.add_argument("--lambd", type=float, default=LAMBDA)
+parser.add_argument("--t0", type=float, default=T0)
+parser.add_argument("--omega", type=float, default=OMEGA)
+parser.add_argument("--opaque", type=float, default=OPAQUE)
+parser.add_argument("--gamma", type=float, default=.6)
 parser.add_argument("--resize", type=int, help="Size of the largest side", default=1000)
 parser.add_argument("--save_folder", "-s", type=str, help="Folder to save haze-free image in", default="./results/")
 parser.add_argument("--soft_matting", "-m", action='store_true', help="Boolean to use soft matting")
@@ -26,12 +30,15 @@ haze_remover = HazeRemover(
     patch_size=args.patch_size,
     use_soft_matting=args.soft_matting,
     guided_image_filtering=args.guided_filtering,
-    lambd=args.lambd
+    lambd=args.lambd,
+    t0=args.t0,
+    omega=args.omega,
+    opaque=args.opaque
 )
-radiance, transmission, _ = haze_remover.remove_haze(.6)
+radiance, transmission, _ = haze_remover.remove_haze(args.gamma)
 
 
 extension = get_save_extension(args.soft_matting, args.guided_filtering, args.resize, file_extension)
 plt.imsave(save_folder + f"{name}_original.{file_extension}", image)
 plt.imsave(save_folder + f"{name}_radiance_{extension}", radiance)
-plt.imsave(save_folder + f"{name}_transmission_{extension}", transmission)
+plt.imsave(save_folder + f"{name}_transmission_{extension}", transmission, cmap='gray')
